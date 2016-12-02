db4f3a17482bfc72f88e66325053d600076b7335
pbprograms <pbprograms@yahoo.com>
2016-12-02 13:29:44 -0500

    static done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def zoo
-  	render html: "Shame on you. When you..."
+  	render html: "Shame on you, when..."
   end
 end
